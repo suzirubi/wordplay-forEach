66ac7b63967a5e1adb3f4607a3e61e8477f7be96
Steve Burton and Suzi Rubino <suzi@>
2016-10-25 19:46:05 -0700

    add arguments

--- js/scripts.js
+++ js/scripts.js
@@ -4,36 +4,18 @@ $(document).ready(function() {
 
     var userSentence = $("input#sentence").val();
     var sentenceSplit = userSentence.split(" ");
-    console.log(sentenceSplit);
-    var sentenceString = sentenceSplit.toString("");
-    console.log(sentenceString);
     var mixSentence = [];
 
-    for (var count = 0; count < count.length; count += 1) {
-      var mixSentence = sentenceSplit.map(function(count, letter) {
-        if (count === "a") {
-          return "-";
-        } else if (count === "e") {
-          return "-";
-        } else if (count === "i") {
-          return "-";
-        } else if (count === "o") {
-          return "-";
-        } else if (count === "u") {
-          return "-";
-        } else if (count === "y") {
-          return "-";
-        } else {
-          return count;
-        }
-      });
+    for (var count = 0; sentenceSplit[].length <= 3; count += 1) {
+      mixSentence.push(sentenceSplit[count]);
+
     }
 
-    var puzzleSentence = mixSentence.join("");
-
-    $("#output").show();
-    $("#puzzleDisplay").text(puzzleSentence);
-    $("form#puzzle").hide();
+    // var puzzleSentence = mixSentence.join("");
+    //
+    // $("#output").show();
+    // $("#puzzleDisplay").text(puzzleSentence);
+    // $("form#puzzle").hide();
     event.preventDefault();
   });
 
